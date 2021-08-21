38f62e9e03d3913d1cb1d9077233537e9927caa7
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2021-08-21 13:36:20 +0530

    add commets

--- app/http/controllers/admin/statusController.js
+++ app/http/controllers/admin/statusController.js
@@ -3,12 +3,13 @@ const order = require('../../../models/order')
 
 function statusController(){
     return{
+              /* order_id and status details get recived  */
         update(req,res){
           order.updateOne({_id: req.body.orderId}, {status: req.body.status},(err,data)=>{
             if(err){
                return res.redirect('/admin/orders') 
             }  
-            // Emit event
+            // Emit event which update the changes made in order update socket
             const eventEmitter = req.app.get('eventEmitter')
             eventEmitter.emit('orderUpdated', {id: req.body.orderId, status: req.body.status})
             res.redirect('/admin/orders')
