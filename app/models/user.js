922bf6b35ad10fafbe51098004ff6b0716a6a890
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2021-08-21 13:57:08 +0530

    add comments

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema
-
+/*
+    schema created for menu page 
+    *** the item which is neccessary can be written by required : true ***
+*/
 const userSchema = new Schema({
     name:{
         type:String,
         required:true,
         unique:true,
     },    
+     // email_id unique for every user; 
     email:{
         type:String,
         required:true,
@@ -23,6 +27,7 @@ const userSchema = new Schema({
     }
 },{timestamps:true})
 
+/* creates a model of userschema as User which use for export */
 const User = mongoose.model('User',userSchema)
 
 module.exports = User;
