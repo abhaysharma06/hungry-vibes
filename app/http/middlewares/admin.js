2a93dfc5becc0d6dc3c68ef6fa7babeda1ed08e8
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2021-08-21 13:46:46 +0530

    add comments

--- app/http/middlewares/admin.js
+++ app/http/middlewares/admin.js
@@ -1,7 +1,13 @@
+/* middleware is create that if user is admin then can move to next page of as admin can access */
 function admin(req,res,next){
+   /*   
+        if user try to login with admin username
+        and user is real/Authentic the move toward the admin access page  
+   */
     if(req.isAuthenticated() && req.user.role == 'admin' ){
         return next()
     }
+   // else redirect to login page
     return res.redirect('/login')
 }
 
