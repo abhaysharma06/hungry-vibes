f08d275593af44e7b32b742c4a51a6424ac5acfd
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2021-08-21 13:43:33 +0530

    add comments

--- app/http/controllers/customers/ordercontroller.js
+++ app/http/controllers/customers/ordercontroller.js
@@ -4,33 +4,58 @@ const { session } = require('passport')
 function ordercontroller (){
     return{
         store(req,res){
-           // validate request
+            
+            /* 
+               check weather the adress and 
+               phone is validate for confirm the step.
+            */
+            
            const {phone, address} = req.body
            if(!phone || !address){
                req.flash('error','All field are required')
                return res.redirect('/cart')
            }
-           const order = new Order({
+            
+           /* order details stores in new new object  */ 
+           
+            const order = new Order({
                customerId: req.user._id,
                items: req.session.cart.items,
                phone: phone,
                address: address
            })
-           order.save().then(result=>{
-             Order.populate(result,{path: 'customerId'},(err,placedOrder)=>{
-                req.flash('success', 'Order placed successfully')
+           
+            /*  
+                order details are saved and when succes 
+                display msg of success order 
+            */
+            
+            order.save().then(result=>{
+            Order.populate(result,{path: 'customerId'},(err,placedOrder)=>{
+            req.flash('success', 'Order placed successfully')
+            
+                // delete session/orders details from cart page      
                 delete req.session.cart
-                // Emit 
+                
+                // Emit event when order is placed, then 
+                //redirects to page where customer can trace order using sockect.io 
                 const eventEmitter = req.app.get('eventEmitter')
                 eventEmitter.emit('orderPlaced', placedOrder)
                 return res.redirect('/customers/orders')
              })
-
+        /* 
+           if something went wrong the trow 
+           error msg and redirect to cart page
+        */
            }).catch(err=>{
                req.flash('error', 'Something Went Wrong')
                return res.redirect('/cart')
            })
         },
+        /* 
+            Customer can view generated order_id with
+            the time of order made(using *** moment***)
+        */
         async index(req,res){
             const orders = await Order.find({ customerId: req.user._id }, 
                 null, {sort:{'createdAt': -1}})
@@ -40,7 +65,7 @@ function ordercontroller (){
         async show(req, res){
           const order = await Order.findById(req.params.id)
 
-          // Authorize user
+     /* only the autherized user can access this page */
           if(req.user._id.toString() === order.customerId.toString()){
               res.render('customers/singleOrder', {order: order})
           }else{
