42d7be23f9647475796e3444f8bdfb81dd12b964
Abhay Sharma <74869287+codeforgrow@users.noreply.github.com>
2021-08-21 13:34:51 +0530

    add comments

--- app/http/controllers/admin/orderController.js
+++ app/http/controllers/admin/orderController.js
@@ -4,6 +4,11 @@ const Order = require('../../../models/order')
 function orderController() {
     return {
         index(req, res) {
+            
+            /* 
+               when order status is ****COMPLETED**** then details of order
+               get vanished from the table generated at time of success order 
+            */
             order.find({ status: { $ne: 'completed' } },null, { sort: { 'createdAt': -1 }}).populate('customerId', '-password').exec((err, orders) => {
                if(req.xhr) {
                    return res.json(orders)
